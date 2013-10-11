e35ceadff4387e9ccb16dfa258fb62fe8aec8297
Visnupriya R <visnupriyar@spritle.com>
2013-10-11 16:27:40 +0530

    Removed orientation folder and added new user folder

--- app/User/user.rb
+++ app/User/user.rb
@@ -1,9 +1,9 @@
 # The model has already been created by the framework, and extends Rhom::RhomObject
 # You can add more methods here
-class Rotation
+class User
   include Rhom::PropertyBag
 
-  # Uncomment the following line to enable sync with Rotation.
+  # Uncomment the following line to enable sync with User.
   # enable :sync
 
   #add model specific code here
